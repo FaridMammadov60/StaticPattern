420d9eb9a23e20f280e358344bc3941b9f155e1c
FaridMammadov60 <faridmma@code.edu.az>
2022-03-28 23:21:16 -0700

    add first commit

---
+++ ConsoleApp1/ConsoleApp1/AppCore/RegularExtention.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.AppCore
+{
+    
+    public static partial class Extention
+    {
+        public const string Email_Pattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+        public const string Phone_Pattern = @"^(?<olkekodu>\+994|0)(?<operator>50|51||55|60|70|77|99)-(\d{3}-\d{2}-\d{2})$";
+        public static bool isEmail(this string email)
+        {
+            string emailPattern = @"";
+            Match emailResult= Regex.Match(email, emailPattern);
+            if (emailResult.Success)
+                return true;
+            return false;
+        }
+        public static bool isPhone(this string phone)
+        {
+            string phonePattern = @"";
+            Match phoneResult = Regex.Match(phone, phonePattern);
+            if (Regex.IsMatch(phone, phonePattern))
+                return true;                
+            return false;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using ConsoleApp1.AppCore.Extention;
+
+
+namespace VirtualExample
+{   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex("");
+            string mobilNumber = "+99450-671-99-99";
+            string mobilNumber2 = "050-671-99-99";
+            string mobilNumber3 = "0050-671-99-99";
+            
+            string email = "farid.Mammadov60@gmail";
+            string email2 = "farid.mammadov60@gmail.com";
+            string email3 = "farid.mammadov60gmail.com";
+            Console.WriteLine(Extention.IsEmail(email));
+        }
+      
+    }
+}
